2fb53e3360882c81ed93592f28986f0ee8c31e41
Elcio Cestari Taira <elciotaira@gmail.com>
2020-09-18 22:34:39 -0400

    This is the first commit. It is just show a app with an activity and no funcionality.

---
+++ app/src/main/java/com/elcio/testsqlite/MainActivity.java
@@ -0,0 +1,14 @@
+package com.elcio.testsqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/elcio/testsqlite/banco/Banco.java
@@ -0,0 +1,9 @@
+package com.elcio.testsqlite.banco;
+
+/**
+ * @author elcio cestari taira
+ * <p/>
+ * <bold>description</bold> - this class represents the main data base functionality
+ */
+public class Banco {
+}
